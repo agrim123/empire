0d4c0bd2a862345225d2a23fa24cd413bd0d5b3d
Agrim Mittal <16553016+agrim123@users.noreply.github.com>
2021-01-10 20:54:41 +0530

    (network_acl) Allow from all to internal

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## empire
+
+This repo contains 
+
+
+### References
+
+- https://aws.amazon.com/blogs/architecture/one-to-many-evolving-vpc-design/
--- network_acl.tf
+++ network_acl.tf
@@ -22,104 +22,14 @@ resource "aws_default_network_acl" "production-network-acl-main" {
   subnet_ids = [
     aws_subnet.production-external-1a.id,
     aws_subnet.production-external-1b.id,
-    aws_subnet.production-external-1c.id
-  ]
-
-  tags = {
-    "Name"        = "production-network-acl-main"
-    "Environment" = "production"
-  }
-}
-
-resource "aws_network_acl" "production-network-acl-internal-internal-1a" {
-  vpc_id = aws_vpc.production.id
-
-  ingress {
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = aws_subnet.production-external-1a.cidr_block
-    protocol   = "all"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  egress {
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    protocol   = "all"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  subnet_ids = [
-    aws_subnet.production-internal-1a.id
-  ]
-
-  tags = {
-    "Name"        = "production-network-acl-internal-1a"
-    "Environment" = "production"
-  }
-}
-
-resource "aws_network_acl" "production-network-acl-internal-1b" {
-  vpc_id = aws_vpc.production.id
-
-  ingress {
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = aws_subnet.production-external-1b.cidr_block
-    protocol   = "all"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  egress {
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    protocol   = "all"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  subnet_ids = [
-    aws_subnet.production-internal-1b.id
-  ]
-
-  tags = {
-    "Name"        = "production-network-acl-internal-1b"
-    "Environment" = "production"
-  }
-}
-
-resource "aws_network_acl" "production-network-acl-internal-1c" {
-  vpc_id = aws_vpc.production.id
-
-  ingress {
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = aws_subnet.production-external-1c.cidr_block
-    protocol   = "all"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  egress {
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    protocol   = "all"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  subnet_ids = [
+    aws_subnet.production-external-1c.id,
+    aws_subnet.production-internal-1a.id,
+    aws_subnet.production-internal-1b.id,
     aws_subnet.production-internal-1c.id
   ]
 
   tags = {
-    "Name"        = "production-network-acl-internal-1c"
+    "Name"        = "production-network-acl-main"
     "Environment" = "production"
   }
 }
